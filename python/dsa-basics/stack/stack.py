afdb072ca912a464c7f647f4c66183f534a83472
iamcodingabit <notnicekasual@gmail.com>
2026-06-26 12:02:32 +0700

    Did a very basic input UI, hit all functions: push, pop, size, peek, empty, quit

--- python/dsa-basics/stack/stack.py
+++ python/dsa-basics/stack/stack.py
@@ -16,12 +16,31 @@ def isEmpty():
 
 def peek():
     return my_array[-1]
-    
-push(17)
-push(48)
-print(my_array)
-pop()
-print(my_array)
-print(size())
-print(isEmpty())
-print(peek())
+
+def main():
+    while True:
+        print(f"Your current array: {my_array}")
+        user_input = input("What you wanna do?: ")
+        user_command = user_input.split()
+        print(f"You said: {user_command}")
+        
+        match user_command[0]:
+            case "push":
+                push(user_command[1])
+            case "pop":
+                pop()
+            case "size":
+                print(size())
+            case "peek":
+                print(peek())
+            case "empty":
+                if isEmpty():
+                    print("Your array is Empty")
+                else:
+                    print("Your array is Not Empty")
+            case "quit":
+                return False
+            case _:
+                print("Can't recognize command")
+        
+main()
